5e4b530756ee896506dbde2fa03d7198a7f7036e
sfekadu <solomonfekadu@hotmail.com>
2021-05-26 00:49:30 +0100

    2 files modified

--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -8,13 +8,14 @@ import { TASKS } from './mock-tasks';
 @Injectable()
 export class TaskService {
   private apiUrl = 'http://localhost:5000/tasks';
-  private dbFile= '../db.json';
+  private dbFile= 'db.json';
 
   constructor(private http: HttpClient) {}
 
   getTasks(): Observable<Task[]> {
     const tasks = of(TASKS);
     return tasks;
+    
     //return this.http.get<Task[]>(this.apiUrl);
     //return this.http.get<Task[]>(this.dbFile);
   }
