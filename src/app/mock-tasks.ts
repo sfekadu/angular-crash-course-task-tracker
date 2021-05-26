14698b217fd6fc2e795e881cf8edcf1ef5381f21
sfekadu <solomonfekadu@hotmail.com>
2021-05-26 16:32:11 +0100

    2 files modified

--- src/app/mock-tasks.ts
+++ src/app/mock-tasks.ts
@@ -19,4 +19,10 @@ export const TASKS: Task[] = [
     day: 'May 7th at 12:30pm',
     reminder: false,
   },
+  {
+    id: 4,
+    text: 'Go to Dentist (SF)',
+    day: 'May 8th at 12:30pm',
+    reminder: false,
+  },
 ];
