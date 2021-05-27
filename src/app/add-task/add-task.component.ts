f978b64a753714985987dca5e5aa4030ca95faa0
sfekadu <solomonfekadu@hotmail.com>
2021-05-27 09:19:08 +0100

    2 files modified

--- src/app/add-task/add-task.component.ts
+++ src/app/add-task/add-task.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Task } from '../Task';
 
 @Component({
   selector: 'app-add-task',
@@ -6,13 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-task.component.css']
 })
 export class AddTaskComponent implements OnInit {
+  @Output() onAddTask: EventEmitter<Task>=new EventEmitter();
   text: string;
   day: string;
   reminder: boolean = false;
-  
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(){
+    if(!this.text)
+    {
+      alert("please ad a task");
+      return;
+    }
+
+    const newTask = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder
+    }
+
+    //@todo - emit EventEmitter
+
+    this.text="";
+    this.day="";
+    this.reminder=false;
   }
 
 }
